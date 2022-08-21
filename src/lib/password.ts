0469e439f05be8767018c8984ee4215f0bb26c6b
Daniel Reszka <dannn.r@gmail.com>
2022-08-21 21:38:44 +0200

    lib/passwords.ts: add with hash() and compare()

---
+++ src/lib/password.ts
@@ -0,0 +1,6 @@
+import bcrypt from 'bcrypt'
+import { dev } from '$app/env'
+
+export const compare = bcrypt.compare
+
+export const hash = async (password: string) => await bcrypt.hash(password, dev ? 1 : 10)
