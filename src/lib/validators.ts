9191b06265c0293c51f192b447ac9f3e932e8388
Daniel Reszka <dannn.r@gmail.com>
2022-08-29 23:11:31 +0200

    validators.ts: add newUserFromCode

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -1,5 +1,11 @@
 export type FormError = { field: string; message: string }
 export type NewUserData = { username: string; email: string; password: string; password2: string }
+export type NewUserFromCodeData = {
+	username: string
+	password: string
+	password2: string
+	code: string
+}
 
 export const newUser = async (newUserData: NewUserData) => {
 	let errors: FormError[] = []
@@ -7,6 +13,17 @@ export const newUser = async (newUserData: NewUserData) => {
 
 	if (username.length < 3) errors = [...errors, { field: 'username', message: 'Too short' }]
 	if (email.length < 3) errors = [...errors, { field: 'email', message: 'Too short' }]
+	if (password.length < 3) errors = [...errors, { field: 'password', message: 'Too short' }]
+	if (password !== password2) errors = [...errors, { field: 'password2', message: "Doesn't match" }]
+	return errors
+}
+
+export const newUserFromCode = async (NewUserFromCodeData: NewUserFromCodeData) => {
+	let errors: FormError[] = []
+	const { username, password, password2, code } = NewUserFromCodeData
+
+	if (!code) errors = [...errors, { field: 'code', message: 'Invitation code missing or invalid' }]
+	if (username.length < 3) errors = [...errors, { field: 'username', message: 'Too short' }]
 	if (password.length < 3) errors = [...errors, { field: 'password', message: 'Too short' }]
 	if (password !== password2) errors = [...errors, { field: 'password2', message: "Doesn't match" }]
 	return errors
