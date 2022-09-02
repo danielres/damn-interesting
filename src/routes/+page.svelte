97871b48ee9cdb6d152273834c4034c2e6a1d7d7
Daniel Reszka <dannn.r@gmail.com>
2022-09-02 13:20:51 +0200

    homepage: styling: improve video entries list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,30 +7,44 @@
 	const promises = entries.map((entry) => YoutubeGetVideoDetails(entry.url))
 	const resolved = Promise.all(promises)
 
-	const WIDTH = 400
+	const WIDTH = 500
 </script>
 
-<h1>Damn interesting: latest</h1>
+<div class="flex justify-center">
+	{#await resolved then entries}
+		<div class="entries">
+			{#each entries as entry}
+				{@const ratio = entry.width / entry.height}
+				{@const id = entry.thumbnail_url.split('/vi/')[1].split('/')[0]}
 
-{#await resolved then entries}
-	{#each entries as entry}
-		{@const ratio = entry.width / entry.height}
-		{@const id = entry.thumbnail_url.split('/vi/')[1].split('/')[0]}
-
-		<div>
-			<h3>
-				{entry.title}
-				<small>by <a href={entry.author_url} target="_BLANK">{entry.author_name}</a></small>
-			</h3>
+				<div class="my-16">
+					<h3 class="flex items-baseline justify-between">
+						<div class="title opacity-75">
+							{entry.title}
+						</div>
+						<div class="text-sm opacity-50">
+							<a class="hover:underline" href={entry.author_url} target="_BLANK">
+								{entry.author_name}
+							</a>
+						</div>
+					</h3>
+					<iframe
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+						allowfullscreen
+						frameborder="0"
+						height={WIDTH / ratio}
+						src={`https://www.youtube.com/embed/${id}?feature=oembed`}
+						title="Top 10 Knots To Know"
+						width={WIDTH}
+					/>
+				</div>
+			{/each}
 		</div>
-		<iframe
-			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-			allowfullscreen
-			frameborder="0"
-			height={WIDTH / ratio}
-			src={`https://www.youtube.com/embed/${id}?feature=oembed`}
-			title="Top 10 Knots To Know"
-			width={WIDTH}
-		/>
-	{/each}
-{/await}
+	{/await}
+</div>
+
+<style>
+	.entries {
+		width: 500px;
+	}
+</style>
