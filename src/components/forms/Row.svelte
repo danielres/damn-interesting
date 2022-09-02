a34d101128f164297f768c880e6747ef073868ec
Daniel Reszka <dannn.r@gmail.com>
2022-09-02 13:04:58 +0200

    forms/Row.svelte: styles: simplify

--- src/components/forms/Row.svelte
+++ src/components/forms/Row.svelte
@@ -1,26 +1,3 @@
-<div class="row">
+<div class="row mb-3">
 	<slot />
 </div>
-
-<style>
-	.row {
-		margin-bottom: 10px;
-	}
-
-	.row :global(input) {
-		border: none;
-		background: #eee;
-		padding: 0.5em 0.75em;
-	}
-
-	.row :global(button) {
-		border: none;
-		padding: 0.5em 0.75em;
-		cursor: pointer;
-		background: #ccc;
-		opacity: 0.7;
-	}
-	.row :global(button):hover {
-		opacity: 1;
-	}
-</style>
