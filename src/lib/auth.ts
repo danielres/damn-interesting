73816eaa34d1f6d7be1da8e64f7c3c7236449608
Daniel Reszka <dannn.r@gmail.com>
2022-09-15 17:37:04 +0200

    lib/auth.ts: add with attemptAuthRestore() + signOut()

---
+++ src/lib/auth.ts
@@ -0,0 +1,18 @@
+import '../app.postcss'
+
+import { browser } from '$app/env'
+import { get } from 'svelte/store'
+import { userStore } from '../stores/user'
+
+export const attemptAuthRestore = async () => {
+	if (!browser) return
+	if (get(userStore)) return
+	const response = await fetch('/auth/restore', { method: 'POST', body: '{}' })
+	if (!response.ok) return
+	const result = await response.json()
+	userStore.set(result)
+}
+
+export const signOut = async () => {
+	userStore.set(undefined)
+}
