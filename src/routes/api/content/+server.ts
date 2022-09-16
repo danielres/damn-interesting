93109ace86c5a6feb34b245ab01c13cfb184fd91
Daniel Reszka <dannn.r@gmail.com>
2022-09-16 18:16:59 +0200

    Adding an entry: fix hardcoded ownerId + restrict to authenticated

--- src/routes/api/content/+server.ts
+++ src/routes/api/content/+server.ts
@@ -1,8 +1,15 @@
+import type { Locals } from 'src/routes/types'
 import type { RequestHandler } from './$types'
-import { Entries } from '../../../db/db'
 
-export const POST: RequestHandler = async ({ request }) => {
+import { Entries } from '../../../db/db'
+import { makeUnauthorizedResponse } from '../../../lib/response'
+
+export const POST: RequestHandler = async ({ locals, request }) => {
+	const { user } = locals as Locals
+	if (!user) return makeUnauthorizedResponse('Unauthorized: User not found')
+
 	const { url, description } = await request.json()
-	const result = await Entries.insert({ url, description, ownerId: 'TODO' })
-	return new Response(JSON.stringify(result))
+	const result = await Entries.insert({ url, description, ownerId: user.id })
+	const { ownerId, ...rest } = result // eslint-disable-line @typescript-eslint/no-unused-vars
+	return new Response(JSON.stringify(rest))
 }
