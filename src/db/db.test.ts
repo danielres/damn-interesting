68c85bcb73ff5da486664ecd0ee9f1839513df48
Daniel Reszka <dannn.r@gmail.com>
2022-09-17 13:44:58 +0200

    db.test.ts: mock getYoutubeGetVideoDetails

--- src/db/db.test.ts
+++ src/db/db.test.ts
@@ -1,8 +1,35 @@
-import { beforeEach, describe, expect, it } from 'vitest'
+import type { YoutubeVideoDetails } from '$lib/Entry/types'
+
+import { afterAll, beforeEach, describe, expect, it, vi } from 'vitest'
 import { delDbFile, Entries, getDb, initialState, Users } from './db'
+
+const fixtures: {
+	youtubeVideoDetails: YoutubeVideoDetails
+} = {
+	youtubeVideoDetails: {
+		authorName: '[AUTHOR-NAME]',
+		authorUrl: '[AUTHOR-URL]',
+		height: 100,
+		providerName: 'YouTube',
+		thumbnailHeight: 100,
+		thumbnailUrl: 'https://i.ytimg.com/vi/1G72936Y3xA/hqdefault.jpg',
+		thumbnailWidth: 100,
+		title: 'Lorem',
+		resourceType: 'video',
+		width: 100,
+	},
+}
+
+vi.mock('../lib/Entry/youtube', () => ({
+	getYoutubeGetVideoDetails: async () => fixtures.youtubeVideoDetails,
+}))
 
 beforeEach(async () => {
 	await delDbFile()
+})
+
+afterAll(() => {
+	vi.restoreAllMocks()
 })
 
 describe('getDb()', () => {
@@ -14,7 +41,7 @@ describe('getDb()', () => {
 })
 
 describe('Users.insert(), Users.list()', () => {
-	it.only('inserts then retrieves a user', async () => {
+	it('inserts then retrieves a user', async () => {
 		const userData = {
 			username: 'Tom',
 			email: 'tom@example.com',
@@ -31,15 +58,23 @@ describe('Users.insert(), Users.list()', () => {
 
 describe('Entries.insert(), Entries.list()', () => {
 	it('inserts then retrieves an entry', async () => {
+		const userData = {
+			username: 'Tom',
+			email: 'tom@example.com',
+			password: 'pass',
+		}
+		await Users.insert(userData)
+		const [user] = await Users.list()
+
 		const entryData = {
-			url: 'URL',
-			ownerId: '123',
+			url: 'https://youtu.be/123',
+			ownerId: user.id,
 			description: 'Lorem',
 		}
 		await Entries.insert(entryData)
 
 		const [dbEntry] = await Entries.list()
-		expect(dbEntry.url).toEqual('URL')
-		expect(dbEntry.id.length).toEqual(36)
+		expect(dbEntry.url).toEqual(entryData.url)
+		expect(dbEntry.id.length).toEqual(11)
 	})
 })
