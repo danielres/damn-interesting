1b5e16f115826521216de19214e75ab23985a020
Daniel Reszka <dannn.r@gmail.com>
2022-09-17 22:21:42 +0200

    Entry.svelte: extract to own file

---
+++ src/components/Entry.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import type { EntryView } from '../db/types'
+	export let entry: EntryView
+
+	const WIDTH = 500
+	const ratio = entry.width / entry.height
+</script>
+
+<div class="my-12">
+	<div class="owner">
+		<span>
+			<a href={`/user/${entry.owner.slug}`}>
+				{entry.owner.username}
+			</a>
+		</span>
+	</div>
+	<h3 class="flex items-baseline justify-between">
+		<div class="title opacity-75">
+			{entry.title}
+		</div>
+		<div class="text-sm opacity-50">
+			<a class="hover:underline" href={entry.authorUrl} target="_BLANK">
+				{entry.authorName}
+			</a>
+		</div>
+	</h3>
+	<iframe
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		allowfullscreen
+		frameborder="0"
+		height={WIDTH / ratio}
+		src={`https://www.youtube.com/embed/${entry.id}?feature=oembed`}
+		title="Top 10 Knots To Know"
+		width={WIDTH}
+	/>
+	<div>{entry.description}</div>
+</div>
+
+<style>
+	.owner span {
+		display: inline-block;
+		background: rgba(0, 0, 0, 0.082);
+		padding: 0.125rem 0.25rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,16 @@
 <script lang="ts">
 	import type { EntryView } from '../db/types'
+	import Entry from '../components/Entry.svelte'
 
 	export let data
 
-	const WIDTH = 500
 	const entries: EntryView[] = data.entries
 </script>
 
 <div class="flex justify-center">
 	<div class="entries">
 		{#each entries as entry}
-			{@const ratio = entry.width / entry.height}
-			{@const id = entry.thumbnailUrl.split('/vi/')[1].split('/')[0]}
-
-			<div class="my-16">
-				<div class="owner">
-					<span>
-						{entry.owner.username}
-					</span>
-				</div>
-				<h3 class="flex items-baseline justify-between">
-					<div class="title opacity-75">
-						{entry.title}
-					</div>
-					<div class="text-sm opacity-50">
-						<a class="hover:underline" href={entry.authorUrl} target="_BLANK">
-							{entry.authorName}
-						</a>
-					</div>
-				</h3>
-				<iframe
-					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-					allowfullscreen
-					frameborder="0"
-					height={WIDTH / ratio}
-					src={`https://www.youtube.com/embed/${id}?feature=oembed`}
-					title="Top 10 Knots To Know"
-					width={WIDTH}
-				/>
-				<div>{entry.description}</div>
-			</div>
+			<Entry {entry} />
 		{/each}
 	</div>
 </div>
@@ -47,11 +18,5 @@
 <style>
 	.entries {
 		width: 500px;
-	}
-
-	.owner span {
-		display: inline-block;
-		background: rgba(0, 0, 0, 0.082);
-		padding: 0.125rem 0.25rem;
 	}
 </style>
