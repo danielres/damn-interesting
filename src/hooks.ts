21e9c33a01381add4635f9ac06f19775f3f16f7c
Daniel Reszka <dannn.r@gmail.com>
2022-09-18 18:55:05 +0200

    hooks.ts: populate event.locals.user

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,8 +1,22 @@
 import type { Handle } from '@sveltejs/kit'
+import type { CurrentUserView, UserDbRecord } from './db/types'
+
+import pick from 'lodash.pick'
+import { populateUser } from './db/db'
 import { getCurrentUserFromRequest } from './lib/request'
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const user = await getCurrentUserFromRequest(event.request)
-	event.locals = { ...event.locals, user }
+	const dbUser: UserDbRecord | undefined = await getCurrentUserFromRequest(event.request)
+
+	if (!dbUser) return resolve(event)
+
+	const currentUser = pick(dbUser, ['id', 'email', 'username', 'slug', 'invitedById', 'invitedAt'])
+	const currentUserView: CurrentUserView = await populateUser(currentUser)
+
+	event.locals = {
+		...event.locals,
+		user: currentUserView,
+	}
+
 	return resolve(event)
 }
