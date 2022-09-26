4c78122e2b4c1ee5a9145e667dbb4e9dd4d0254a
Daniel Reszka <dannn.r@gmail.com>
2022-09-26 22:52:38 +0200

    prisma: migrations: generate first "init" migration

---
+++ prisma/migrations/20220926171032_init/migration.sql
@@ -0,0 +1,48 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "invitedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "inviterId" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Entry" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "url" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "ownerId" TEXT NOT NULL,
+    "authorName" TEXT NOT NULL,
+    "authorUrl" TEXT NOT NULL,
+    "height" INTEGER NOT NULL,
+    "providerName" TEXT NOT NULL,
+    "thumbnailHeight" INTEGER NOT NULL,
+    "thumbnailUrl" TEXT NOT NULL,
+    "thumbnailWidth" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "resourceType" TEXT NOT NULL,
+    "width" INTEGER NOT NULL,
+
+    CONSTRAINT "Entry_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_slug_key" ON "User"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_inviterId_fkey" FOREIGN KEY ("inviterId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Entry" ADD CONSTRAINT "Entry_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
