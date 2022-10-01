c3a91cd4fe3b3cf2b24aacc7fdad2f2bec9d3e99
Daniel Reszka <dannn.r@gmail.com>
2022-10-01 13:42:24 +0200

    validators:ts: password: require length >=8 in non-dev

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -1,4 +1,7 @@
 export type FormError = { field: string; message: string }
+import { dev } from '$app/environment'
+
+const minPwLength = dev ? 4 : 8
 
 const serializeFormData = (formData: FormData): { [k: string]: string } =>
 	JSON.parse(JSON.stringify(Object.fromEntries(formData.entries())))
@@ -9,7 +12,8 @@ export const newUser = (formData: FormData) => {
 	const errors: FormError[] = []
 	if (username.length < 3) errors.push({ field: 'username', message: 'Too short' })
 	if (email.length < 3) errors.push({ field: 'email', message: 'Too short' })
-	if (password.length < 3) errors.push({ field: 'password', message: 'Too short' })
+	if (password.length < minPwLength)
+		errors.push({ field: 'password', message: `Too short (${minPwLength} chars min)` })
 	if (password !== password2) errors.push({ field: 'password2', message: "Doesn't match" })
 	return errors
 }
@@ -20,7 +24,8 @@ export const newUserFromCode = (formData: FormData) => {
 	const errors: FormError[] = []
 	if (!code) errors.push({ field: 'code', message: 'Invitation code missing or invalid' })
 	if (username.length < 3) errors.push({ field: 'username', message: 'Too short' })
-	if (password.length < 3) errors.push({ field: 'password', message: 'Too short' })
+	if (password.length < minPwLength)
+		errors.push({ field: 'password', message: `Too short (${minPwLength} chars min)` })
 	if (password !== password2) errors.push({ field: 'password2', message: "Doesn't match" })
 	return errors
 }
