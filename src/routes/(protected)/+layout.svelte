bb4e349a4e58e23e267d54200b01be0d3ee714bd
Daniel Reszka <dannn.r@gmail.com>
2022-10-02 20:13:37 +0200

    layout: add logo

--- src/routes/(protected)/+layout.svelte
+++ src/routes/(protected)/+layout.svelte
@@ -21,18 +21,38 @@
 </script>
 
 {#if $userStore}
-	<header class={`flex ${isAdmin ? 'bg-red-900/50' : 'bg-slate-900'} px-8 py-4 items-baseline`}>
-		<h1 class="text-xl flex-grow">
-			<a href="/" class="drop-shadow-sharp hover:text-white transition-colors">Damn Interesting</a>
+	<header class={`flex ${isAdmin ? 'bg-red-900/50' : 'bg-slate-900'} px-6 sm:px-8 py-4`}>
+		<h1 class="">
+			<a href="/" class="drop-shadow-sharp hover:text-white transition-colors flex gap-2 ">
+				<svg
+					class="h-8 w-8 opacity-70 self-end"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						style="fill:none;stroke-width:1.42247;stroke-dasharray:none;stroke-opacity:1;stroke-linecap:round;paint-order:markers fill stroke;stroke-linejoin:round"
+						d="M3.3 3.4C2 3.4 1 4.4 1 5.6v9.7c0 1.3 1 2.3 2.2 2.3h17.5c1.2 0 2.2-1 2.2-2.3V5.6c0-1.2-1-2.2-2.2-2.2H3.3zM9.6 6l3.7 2.2 3.8 2.2-3.8 2.1-3.7 2.2V6.1zM12 19a1.6 1.6 0 0 0-1.5 1.6 1.6 1.6 0 0 0 1.5 1.5 1.6 1.6 0 0 0 1.6-1.5A1.6 1.6 0 0 0 12 19zm-9.7 1.6H10 2.3zm11.8 0h7.7H14z"
+					/>
+				</svg>
+
+				<div class="leading-none">
+					<div class="text-sm opacity-70">Damn</div>
+					<div>Interesting</div>
+				</div>
+			</a>
 		</h1>
 
-		<div class="ml-auto flex space-x-8">
-			<a class="hover:underline underline-offset-4 hover:text-white transition-colors" href="/me"
+		<div class="ml-auto flex space-x-8  self-end leading-none">
+			<a
+				class="drop-shadow-sharp hover:underline underline-offset-4 hover:text-white transition-colors"
+				href="/me"
 				>{$userStore.username}
 			</a>
 
 			<form
-				class="inline-block"
+				class="drop-shadow-sharp inline-block"
 				use:enhance={() => () => ($userStore = undefined)}
 				action="/auth?/signout"
 				method="POST"
