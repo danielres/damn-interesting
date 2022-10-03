696c089b1d9b42e046f8b1bf2570ff6f5acfcc8e
Daniel Reszka <dannn.r@gmail.com>
2022-10-03 23:17:25 +0200

    lib/can: add can.replay

--- src/lib/can.ts
+++ src/lib/can.ts
@@ -2,8 +2,9 @@ import type { PrismaClient } from '@prisma/client'
 
 export type Can = ReturnType<typeof can>
 
-export const can: Can = (prisma: PrismaClient) => {
+export const can = (prisma: PrismaClient) => {
 	return {
-		signup: async () => (await prisma.user.count({})) === 0,
+		signup: async () => (await prisma.user.count()) === 0,
+		replay: async () => (await prisma.user.count()) === 0,
 	}
 }
