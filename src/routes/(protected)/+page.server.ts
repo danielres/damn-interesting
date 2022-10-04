62cee29fe7f0cec203e5866d53d6d661239dd8fc
Daniel Reszka <dannn.r@gmail.com>
2022-10-04 22:11:31 +0200

    homepage: order entries by createdAt DESC

--- src/routes/(protected)/+page.server.ts
+++ src/routes/(protected)/+page.server.ts
@@ -3,6 +3,7 @@ import type { PageServerLoad } from './$types'
 export const load: PageServerLoad = async ({ locals }) => {
 	const entries = await locals.prisma.entry.findMany({
 		include: { owner: { select: { username: true, slug: true } } },
+		orderBy: { createdAt: 'desc' },
 	})
 
 	return { entries }
