908a1cb2d76c4274c20bf4a6be8a7652d6ade0e5
Daniel Reszka <dannn.r@gmail.com>
2022-10-05 00:43:30 +0200

    homepage: add ButtonSubmitContentWithModal

--- src/routes/(protected)/+page.svelte
+++ src/routes/(protected)/+page.svelte
@@ -2,6 +2,7 @@
 	import type { EntryView } from '$types'
 
 	import Entry from '$components/Entry.svelte'
+	import ButtonSubmitContentWithModal from '$components/ButtonSubmitContentWithModal.svelte'
 
 	export let data
 
@@ -9,6 +10,8 @@
 </script>
 
 <div class="py-12 px-2 sm:px-8 max-w-7xl mx-auto">
+	<ButtonSubmitContentWithModal />
+
 	<div class="grid gap-8">
 		{#each entries as entry}
 			<div class="card p-4 md:p-6 hover:shadow-2xl hover:bg-slate-600 transition-all">
