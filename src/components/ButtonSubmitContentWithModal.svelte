925a33ac1dbf902b240165dddb1c4fa9e85d62b3
Daniel Reszka <dannn.r@gmail.com>
2022-10-05 00:42:23 +0200

    copmonents: add ButtonSubmitContentWithModal

---
+++ src/components/ButtonSubmitContentWithModal.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { invalidateAll } from '$app/navigation'
+	import FormSubmitContent from '$components/forms/FormSubmitContent.svelte'
+	import Modal from '$components/Modal.svelte'
+</script>
+
+<div class="mb-4 -mt-4 flex justify-center gap-4">
+	<Modal let:open let:close>
+		<button
+			class="flex w-8 h-8 bg-slate-400 hover:bg-slate-200 transition-colors text-slate-900 text-3xl items-center justify-center rounded-full"
+			title="Add content"
+			on:click={open}
+		>
+			+
+		</button>
+
+		<div slot="content" class="grid gap-4">
+			<h3 class="text-xl">Submit new content</h3>
+			<FormSubmitContent
+				onSuccess={() => {
+					invalidateAll()
+					close()
+				}}
+			/>
+		</div>
+	</Modal>
+</div>
