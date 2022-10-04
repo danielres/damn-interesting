ccbad25b6ed73960837634292681acee5123d3f1
Daniel Reszka <dannn.r@gmail.com>
2022-10-05 00:36:54 +0200

    components: add Modal

---
+++ src/components/Modal.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition'
+
+	export let isOpen = false
+	export const open = () => (isOpen = true)
+	export const close = () => (isOpen = false)
+</script>
+
+<slot {open} {close} />
+
+{#if isOpen}
+	<div class="card modal " transition:fade>
+		<slot name="content" />
+	</div>
+
+	<div class="backdrop" transition:fade on:click={() => (isOpen = false)} />
+{/if}
+
+<style lang="postcss">
+	.modal {
+		@apply fixed z-20 max-w-xl md:w-2/3 mx-auto;
+	}
+
+	.backdrop {
+		@apply fixed z-10 top-0 left-0 min-w-full min-h-screen py-16 cursor-pointer;
+		@apply bg-black/50 backdrop-blur;
+	}
+</style>
