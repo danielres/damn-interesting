3de28ef8b87f7793a180cb259447f81ce92dc5ff
Daniel Reszka <dannn.r@gmail.com>
2022-10-05 00:44:33 +0200

    homepage: fix not invalidating entries + fix types

--- src/routes/(protected)/+page.svelte
+++ src/routes/(protected)/+page.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
-	import type { EntryView } from '$types'
+	import type { PageData } from './$types'
 
 	import Entry from '$components/Entry.svelte'
 	import ButtonSubmitContentWithModal from '$components/ButtonSubmitContentWithModal.svelte'
 
-	export let data
-
-	const entries: EntryView[] = data.entries
+	export let data: PageData
 </script>
 
 <div class="py-12 px-2 sm:px-8 max-w-7xl mx-auto">
 	<ButtonSubmitContentWithModal />
 
 	<div class="grid gap-8">
-		{#each entries as entry}
+		{#each data.entries as entry}
 			<div class="card p-4 md:p-6 hover:shadow-2xl hover:bg-slate-600 transition-all">
 				<Entry {entry} mode="thumbnail" />
 			</div>
