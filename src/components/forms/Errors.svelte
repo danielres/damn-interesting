579c510f059130a3dc5bbf29a0880255208a73f0
Daniel Reszka <dannn.r@gmail.com>
2022-10-05 16:27:10 +0200

    Errors: inherit from Alert

--- src/components/forms/Errors.svelte
+++ src/components/forms/Errors.svelte
@@ -1,28 +1,21 @@
-<script lang="ts" scope="module">
+<script lang="ts">
 	import type { FormError } from '$lib/validators'
 
-	import { fade } from 'svelte/transition'
+	import Alert from '$components/Alert.svelte'
 
 	export let errors: FormError[] = []
 </script>
 
-<ul class="errors" transition:fade>
-	{#each errors as error}
-		<li>
-			{#if error.field}
-				<b>{error.field}:</b>
-			{/if}
+<Alert type="warning">
+	<ul>
+		{#each errors as error}
+			<li>
+				{#if error.field}
+					<b>{error.field}:</b>
+				{/if}
 
-			{error.message}
-		</li>
-	{/each}
-</ul>
-
-<style lang="postcss">
-	ul.errors {
-		@apply bg-red-200 rounded border-4 border-red-300;
-		@apply text-red-600 text-sm;
-		@apply px-3 py-2;
-		@apply grid gap-1;
-	}
-</style>
+				{error.message}
+			</li>
+		{/each}
+	</ul>
+</Alert>
