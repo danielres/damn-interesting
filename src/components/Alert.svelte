770c22b53a03a8996a550f5a4c9bc85ce3e611cd
Daniel Reszka <dannn.r@gmail.com>
2022-10-05 16:25:49 +0200

    components: add Alert

---
+++ src/components/Alert.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition'
+
+	export let type: 'warning' | 'success' | undefined = undefined
+</script>
+
+<div class="{type} alert " transition:fade>
+	<slot />
+</div>
+
+<style lang="postcss">
+	.alert {
+		@apply rounded border-4 text-sm;
+		@apply px-3 py-2;
+	}
+
+	.warning {
+		@apply bg-red-200  border-red-300 text-red-600;
+	}
+
+	.success {
+		@apply bg-green-300  border-green-400 text-green-800;
+	}
+</style>
