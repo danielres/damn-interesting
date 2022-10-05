971c404823773effa414a870d9c179c03f3307d4
Daniel Reszka <dannn.r@gmail.com>
2022-10-05 16:39:28 +0200

    can: add UpdateEntry

--- src/lib/can.ts
+++ src/lib/can.ts
@@ -1,10 +1,20 @@
-import type { PrismaClient } from '@prisma/client'
+import { USER_ROLES } from '$constants'
+import type { Entry, PrismaClient } from '@prisma/client'
+type User = App.Locals['user']
 
 export type Can = ReturnType<typeof can>
 
 export const can = (prisma: PrismaClient) => {
 	return {
 		signup: async () => (await prisma.user.count()) === 0,
+
 		replay: async () => (await prisma.user.count()) === 0,
+
+		updateEntry: (user: User | null, entry: Entry | null) => {
+			if (!user) return false
+			if (user.role === USER_ROLES.SUPERADMIN) return true
+			if (entry?.ownerId === user.id) return true
+			return false
+		},
 	}
 }
