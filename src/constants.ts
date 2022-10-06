89204fe1979ad6df6ee41ea2bd149b13719a64d3
Daniel Reszka <dannn.r@gmail.com>
2022-10-06 15:42:49 +0200

    add page About

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+import { PUBLIC_OWNER_SLUG, PUBLIC_OWNER_USERNAME } from '$env/static/public'
+
 export const HTTP_CODES = {
 	FORBIDDEN: 403,
 	UNAUTHORIZED: 401,
@@ -15,4 +17,9 @@ export const COOKIES = {
 		name: 'session',
 		options: { httpOnly: true, sameSite: 'strict', path: '/', secure: true } as const,
 	},
+}
+
+export const OWNER = {
+	slug: PUBLIC_OWNER_SLUG,
+	username: PUBLIC_OWNER_USERNAME,
 }
---
+++ src/routes/(protected)/about/+page.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+	import { OWNER } from '$constants'
+</script>
+
+<div class="max-w-4xl mx-auto my-12 grid gap-8 md:p-8 text-slate-300">
+	<h1 class="px-6">
+		What is this about?
+		<small class="block text-slate-500 text-sm">
+			(If you're reading this, there's a good chance that you already have some idea...)
+		</small>
+	</h1>
+
+	<section class="card">
+		<p>Damn Interesting is a platform to share content privately with friends.</p>
+		<p>
+			This project is
+			<a rel="external" href="https://github.com/danielres/damn-interesting"> open-source </a>
+			and you are welcome to inspect, use and fork the code, and host your own instance if you wish.
+		</p>
+		<p>
+			This particular instance is hosted by <a href="/user/{OWNER.slug}">{OWNER.username}</a>.
+		</p>
+		<p>
+			At the time of this writing, only Youtube videos are supported. However, other types of
+			content might become supported as well over time.
+		</p>
+	</section>
+
+	<section class="card">
+		<h2>Etiquette</h2>
+		<p>
+			Please remain thoughtful and sensitive with the content you share, and with your communication
+			in general. This space is meant to remain always welcoming to all people regardless of their
+			gender, sexual preferences or origins.
+		</p>
+		<p>
+			This instance is dedicated for content that is, in general:
+			<br />In-depth / Enlightening / Inspiring / Educational / Beneficial in some way / ...
+		</p>
+		<p>
+			In the case of videos, any content less than 10 minutes long probably doesn't satisfy those...
+			but it might! Use your judgment! And please don't be offended if something you posted is
+			moderated or removed. You are welcome to host your own private instance if you have a
+			different vision.
+		</p>
+	</section>
+
+	<section class="card">
+		<h2>What to post?</h2>
+		<p>
+			Within the limits of the above etiquette, anything that you find really interesting! It can be
+			totally niche. Maybe you have a passion for knitting, sculpture, philosphy, survivalism, dog
+			training, wild camping,...? This is a place to share YOUR most valued findings on those
+			subjects, and to discover content shared by others.
+		</p>
+		<p>
+			In the future, tags, collections and subscriptions will be implemented, so that each person
+			can discover and find content according to their interests.
+		</p>
+		<p>
+			When you share something, please always provide a short description at least. Tell us what YOU
+			find damn interesting in that piece of content!
+		</p>
+	</section>
+
+	<div class="text-center">
+		<p>Thank you for reading!</p>
+	</div>
+</div>
+
+<style lang="postcss">
+	h1 {
+		@apply text-2xl;
+	}
+
+	h2 {
+		@apply text-xl text-white;
+	}
+
+	p {
+		line-height: 1.4;
+		a {
+			@apply underline underline-offset-4;
+		}
+	}
+
+	section {
+		@apply grid gap-6 bg-slate-900/50;
+	}
+</style>
