2f0cc1bdf95eaf8e26995af4dc452bdc786cc6e5
Daniel Reszka <dannn.r@gmail.com>
2022-10-07 01:08:20 +0200

    Entry: cleanup code

--- src/components/Entry.svelte
+++ src/components/Entry.svelte
@@ -14,19 +14,18 @@
 			{entry.title}
 		</div>
 		<div class="text-sm opacity-50">
-			<a class="underline-offset-4 hover:underline" href={entry.authorUrl} target="_BLANK">
+			<a class="underline-offset-4 hover:underline" href={entry.authorUrl} rel="external">
 				{entry.authorName}
 			</a>
 		</div>
 	</h3>
 
 	{#if mode === 'thumbnail'}
-		<a href={`/entries/${entry.id}`} class="opacity-70 hover:opacity-100" data-sveltekit-prefetch>
+		<a href="/entries/{entry.id}" class="opacity-70 hover:opacity-100" data-sveltekit-prefetch>
 			<img
 				src={entry.thumbnailUrl}
 				alt={entry.title}
 				class="w-full rounded-lg border-4 border-slate-600 hover:border-slate-400 transition-all hover:shadow-lg object-cover aspect-video"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 			/>
 		</a>
 	{/if}
@@ -34,11 +33,9 @@
 	{#if mode === 'view'}
 		<iframe
 			class="w-full rounded-lg border-4 border-slate-600"
-			style={`aspect-ratio: ${entry.width} / ${entry.height}`}
-			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 			allowfullscreen
 			frameborder="0"
-			src={`https://www.youtube.com/embed/${entry.id}?feature=oembed`}
+			src="https://www.youtube.com/embed/{entry.id}?feature=oembed"
 			title="Top 10 Knots To Know"
 		/>
 	{/if}
@@ -47,7 +44,7 @@
 		<div class="flex items-baseline space-x-4">
 			<span class="ml-auto text-sm opacity-50">{format(entry.createdAt)}</span>
 			<span class="mx-4">
-				<a href={`/user/${entry.owner.slug}`} class="hover:underline underline-offset-4">
+				<a href="/user/{entry.owner.slug}" class="hover:underline underline-offset-4">
 					{entry.owner.username}
 				</a>
 			</span>
