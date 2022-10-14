7504a13bd422e75b2f782c687ce12a68ebbca943
Daniel Reszka <dannn.r@gmail.com>
2022-10-14 17:21:50 +0200

    Tags.svelte: cleanup, improve types

--- src/routes/(protected)/entries/[id]/Tags.svelte
+++ src/routes/(protected)/entries/[id]/Tags.svelte
@@ -1,28 +1,29 @@
 <script lang="ts">
-	import type { EntryView } from '$types'
+	import type { Entry, Tag } from '@prisma/client'
 
 	import { enhance } from '$app/forms'
 	import { invalidateAll } from '$app/navigation'
 	import { fade } from 'svelte/transition'
 
 	export let isEditing = false
-	export let entry: EntryView
+	export let entry: {
+		id: Entry['id']
+		taggings: { tag: Tag }[]
+	}
 </script>
 
 <ul class="text-sm flex gap-2 flex-wrap">
-	{#each entry.tags as tag, i (tag.id)}
+	{#each entry.taggings as { tag } (tag.id)}
 		<li class="bg-slate-700 px-2 py-1 rounded-lg flex items-center gap-2" transition:fade>
 			<span>{tag.name}</span>
 			{#if isEditing}
 				<form
 					action="/tags?/disconnect"
 					method="post"
-					use:enhance={({ form, data }) => {
+					use:enhance={({ data }) => {
 						data.set('entryId', entry.id)
 						data.set('tagId', tag.id)
-						return () => {
-							invalidateAll()
-						}
+						return invalidateAll
 					}}
 				>
 					<button
