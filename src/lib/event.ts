6eb71c7321a8c5b751bfc9c6e5097feee76e4046
Daniel Reszka <dannn.r@gmail.com>
2022-10-21 10:57:13 +0200

    lib/event.ts: debounce: disable ts warning "no-explicit-any"

--- src/lib/event.ts
+++ src/lib/event.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const debounce = (cb: (...args: any[]) => void, delay = 250) => {
 	let timeout: NodeJS.Timeout
 	return (...args: unknown[]) => {
