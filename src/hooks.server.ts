402d23681445565349565b1ab3a250024862e5a1
Daniel Reszka <dannn.r@gmail.com>
2022-10-24 23:29:38 +0200

    hooks.server.ts: cache current user result from encrypted session
    * saves 50-100 ms

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,9 +4,13 @@ import { can } from '$can'
 import { decryptObject } from '$lib/encryption'
 import { prisma, prisma2 } from '$lib/prisma/clients'
 import { hashUserPassword, sourceEvents } from '$lib/prisma/middlewares'
+import { Cache } from '$lib/cache'
 
 prisma.$use(hashUserPassword)
 prisma.$use(sourceEvents)
+
+const userCache = new Cache('user', 10, 30)
+
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.prisma = prisma
 	event.locals.prisma2 = prisma2
@@ -15,21 +19,28 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const encryptedSession = event.cookies.get('session')
 	if (!encryptedSession) return resolve(event)
 
-	const { userId } = decryptObject(encryptedSession)
+	let dbUser
 
-	const dbUser = await prisma.user.findFirst({
-		where: { id: userId },
-		select: {
-			id: true,
-			email: true,
-			username: true,
-			role: true,
-			slug: true,
-			inviter: { select: { username: true, slug: true } },
-			invitedAt: true,
-		},
-	})
+	if (userCache.get(encryptedSession)) {
+		dbUser = userCache.get(encryptedSession)
+	} else {
+		const { userId } = decryptObject(encryptedSession)
 
+		dbUser = await prisma.user.findFirst({
+			where: { id: userId },
+			select: {
+				id: true,
+				email: true,
+				username: true,
+				role: true,
+				slug: true,
+				inviter: { select: { username: true, slug: true } },
+				invitedAt: true,
+			},
+		})
+	}
+
+	userCache.set(encryptedSession, dbUser)
 	event.locals.user = dbUser
 
 	return resolve(event)
