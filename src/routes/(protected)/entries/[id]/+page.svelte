3c4d8e75a4fcae90e3bbbc650f06f5e4184bc721
Daniel Reszka <dannn.r@gmail.com>
2022-10-26 20:00:15 +0200

    /entrie/[id]: improve page design

--- src/routes/(protected)/entries/[id]/+page.svelte
+++ src/routes/(protected)/entries/[id]/+page.svelte
@@ -6,9 +6,9 @@
 	import { invalidateAll } from '$app/navigation'
 	import { can } from '$can'
 	import ButtonEdit from '$components/ButtonEdit.svelte'
-	import Entry from '$components/Entry.svelte'
 	import Errors from '$components/forms/Errors.svelte'
 	import { format } from '$lib/date'
+	import Suggestions from './Suggestions.svelte'
 	import Tags from './Tags.svelte'
 
 	export let data: PageData
@@ -23,101 +23,126 @@
 	$: entry = data.entry!
 </script>
 
-<div class="mx-auto grid w-full max-w-5xl gap-16 py-12 md:px-8">
-	<div class="grid">
-		{#if can.updateEntry(data.user, entry)}
-			<ButtonEdit
-				{isEditing}
-				toggle={() => (isEditing = !isEditing)}
-				end={() => (isEditing = false)}
-			/>
-		{/if}
-
-		<form
-			method="POST"
-			action="/entries/{entry.id}?/update"
-			use:enhance={() => {
-				return ({ result }) => {
-					errors = []
-					if (result.type === 'success') {
-						invalidateAll()
-						isEditing = false
-					}
-					if (result.type === 'invalid') errors = result.data?.errors
-					if (result.type === 'error') errors = [result.error]
+<div class="mx-auto w-full max-w-5xl py-6 px-2 md:py-12 md:px-8">
+	<form
+		method="POST"
+		action="/entries/{entry.id}?/update"
+		use:enhance={() => {
+			return ({ result }) => {
+				errors = []
+				if (result.type === 'success') {
+					invalidateAll()
+					isEditing = false
 				}
-			}}
-		>
-			<div class="card grid gap-6 border-2 border-black bg-slate-900">
-				<div class=" grid gap-4">
-					<h1 class="grid gap-2">
-						{#if isEditing}
-							<input class="p-0 text-2xl" type="text" name="title" id="title" bind:value={title} />
-						{:else}
-							<div class="text-2xl text-slate-400">{entry.title}</div>
-						{/if}
-						<div class="flex gap-4 text-sm">
-							<div>
-								<span class="text-slate-400">Created by</span>
-								<a
-									class="inline-block underline-offset-4 hover:underline"
-									href="user/{entry.authorUrl}"
-								>
-									{entry.authorName}
-								</a>
-							</div>
+				if (result.type === 'invalid') errors = result.data?.errors
+				if (result.type === 'error') errors = [result.error]
+			}
+		}}
+	>
+		<div class="grid gap-8">
+			<h1 class="flex items-center text-lg text-slate-400 md:text-2xl">
+				{#if isEditing}
+					<input class="p-0 md:text-2xl" type="text" name="title" id="title" bind:value={title} />
+				{:else}
+					{entry.title}
+				{/if}
+			</h1>
 
-							<div>
-								<span class="text-slate-400">Shared by</span>
-								<a class="underline-offset-4 hover:underline" href="/user/{entry.owner.slug}">
-									{entry.owner.username}
-								</a>
-								<span class="inline-block text-slate-400">on {format(entry.createdAt)}</span>
-							</div>
-						</div>
-					</h1>
+			<div class=" grid gap-4 md:grid-cols-2 md:gap-8">
+				<div class="card h-fit border-2 border-black bg-slate-900">
+					<iframe
+						class="w-full rounded-lg"
+						style="aspect-ratio: {entry.width}/{entry.height}"
+						allowfullscreen
+						frameborder="0"
+						src="https://www.youtube.com/embed/{entry.id}?feature=oembed"
+						title={entry.title}
+					/>
+				</div>
+
+				<div class="grid h-fit gap-6">
+					{#if isEditing}
+						<textarea
+							class="p-0 text-sm"
+							name="description"
+							id="description"
+							rows="3"
+							bind:value={description}
+						/>
+					{:else}
+						<div class="text-sm">{entry.description}</div>
+					{/if}
 
 					<Tags {entry} {isEditing} />
-				</div>
-				<iframe
-					class="w-full rounded-lg"
-					style="aspect-ratio: {entry.width}/{entry.height}"
-					allowfullscreen
-					frameborder="0"
-					src="https://www.youtube.com/embed/{entry.id}?feature=oembed"
-					title={entry.title}
-				/>
 
-				{#if isEditing}
-					<textarea name="description" id="description" rows="3" bind:value={description} />
-				{:else}
-					<div>{entry.description}</div>
-				{/if}
+					<div class="flex gap-4 text-sm">
+						<div>
+							<span class="text-slate-400">Created by</span>
+							<a
+								class="inline-block underline-offset-4 hover:underline"
+								href="user/{entry.authorUrl}"
+							>
+								{entry.authorName}
+							</a>
+						</div>
 
-				{#if isEditing}
-					<div class="mx-auto grid gap-4">
-						{#if errors.length > 0}
-							<Errors {errors} />
-						{/if}
-
-						<button disabled={!ischanged} class="btn mx-auto w-fit" type="submit">Submit</button>
+						<div>
+							<span class="text-slate-400">Shared by</span>
+							<a class="underline-offset-4 hover:underline" href="/user/{entry.owner.slug}">
+								{entry.owner.username}
+							</a>
+							<span class="inline-block text-slate-400">on {format(entry.createdAt)}</span>
+						</div>
 					</div>
-				{/if}
+
+					{#if can.updateEntry(data.user, entry)}
+						<div class="flex gap-4">
+							{#if isEditing}
+								<button
+									type="submit"
+									class="rounded bg-green-600 bg-opacity-30 py-1 px-2 transition-all hover:bg-opacity-50"
+								>
+									Submit
+								</button>
+								<button
+									type="button"
+									on:click={() => (isEditing = false)}
+									class="rounded bg-red-600 bg-opacity-30 py-1 px-2 transition-all hover:bg-opacity-50"
+								>
+									Cancel
+								</button>
+							{:else}
+								<button
+									class="opacity-50 transition-opacity hover:opacity-100"
+									on:click={() => (isEditing = true)}
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										class="h-6 w-6 rounded-full"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+										/>
+									</svg>
+								</button>
+							{/if}
+						</div>
+					{/if}
+				</div>
 			</div>
-		</form>
-	</div>
+		</div>
+	</form>
 
 	{#if data.suggestionsByAuthor.length > 0}
-		<div class="grid gap-4">
-			<h3 class="border-b border-slate-500">Also by {entry.authorName}</h3>
-
-			<div class="grid gap-8 sm:grid-cols-2 md:grid-cols-3">
-				{#each data.suggestionsByAuthor as suggested}
-					<div class="card bg-slate-700 p-6">
-						<Entry entry={suggested} mode="thumbnail" />
-					</div>
-				{/each}
-			</div>
+		<div class="mt-16 grid gap-4">
+			<h3 class="">Also by {entry.authorName}</h3>
+			<Suggestions suggestions={data.suggestionsByAuthor} />
 		</div>
 	{/if}
 </div>
---
+++ src/routes/(protected)/entries/[id]/Suggestions.svelte
@@ -0,0 +1,14 @@
+<script lang="ts" scope="module">
+	import type { PageData } from './$types'
+
+	import Entry from '$components/Entry.svelte'
+	export let suggestions: PageData['suggestionsByAuthor']
+</script>
+
+<div class="grid gap-8 sm:grid-cols-2 md:grid-cols-3">
+	{#each suggestions as suggestion}
+		<div class="card bg-slate-700 p-6">
+			<Entry entry={suggestion} mode="thumbnail" />
+		</div>
+	{/each}
+</div>
