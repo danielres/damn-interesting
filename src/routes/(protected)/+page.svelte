64aa6b08b89bc20af4411886c719de801f7e4800
Daniel Reszka <dannn.r@gmail.com>
2022-11-06 23:07:19 +0100

    homepage: entries: justify center

--- src/routes/(protected)/+page.svelte
+++ src/routes/(protected)/+page.svelte
@@ -9,7 +9,7 @@
 <div class="mx-auto grid max-w-7xl gap-4 py-2 px-2 sm:px-8">
 	<ButtonSubmitContentWithModal />
 
-	<div class="flex flex-wrap gap-8">
+	<div class="flex flex-wrap justify-center gap-8">
 		{#each data.entries as entry}
 			<div class="card p-4 transition-all hover:bg-slate-600 hover:shadow-2xl md:w-96 md:p-6">
 				<Entry {entry} mode="thumbnail" />
