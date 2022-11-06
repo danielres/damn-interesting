9b91adb021221e758e137e0025968568955b4657
Daniel Reszka <dannn.r@gmail.com>
2022-11-06 19:45:45 +0100

    components: add forms/TextareaAutogrow.svelte

---
+++ src/components/forms/TextareaAutogrow.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let id: string
+	export let name: string
+	export let value: string
+</script>
+
+<div class="auto-grow">
+	<textarea {name} {id} bind:value />
+	<span class="replicated">{value + ' '}</span>
+</div>
+
+<style lang="postcss">
+	.auto-grow {
+		display: grid;
+
+		textarea {
+			resize: none;
+			overflow: hidden;
+		}
+
+		.replicated {
+			white-space: pre-wrap;
+			visibility: hidden;
+		}
+
+		textarea,
+		.replicated {
+			@apply p-0 text-sm;
+			grid-area: 1 / 1 / 2 / 2; /* Place on top of each other */
+		}
+	}
+</style>
