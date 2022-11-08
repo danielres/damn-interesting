8044c6ced042a3491492f5e431b838cf38ed9f19
Daniel Reszka <dannn.r@gmail.com>
2022-11-08 01:54:55 +0100

    Modal: fix UX issues
    * fixes modal content unscrollable while covered
       with on-screen keyboard on mobile
    
    * adds a close button

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -13,7 +13,19 @@
 <slot {open} {close} />
 
 {#if isOpen}
-	<div class="card modal" transition:fade>
+	<div class="card modal p-6" transition:fade>
+		<button class="close" on:click={close}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="h-5 w-5"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+			</svg>
+		</button>
 		<slot name="content" />
 	</div>
 
@@ -22,7 +34,12 @@
 
 <style lang="postcss">
 	.modal {
-		@apply fixed z-20 mx-auto max-w-xl md:w-2/3;
+		@apply absolute top-16 z-20 mx-auto max-w-xl md:fixed md:top-auto md:w-2/3;
+	}
+
+	button.close {
+		@apply rounded-full bg-black bg-opacity-0 p-1 hover:bg-opacity-20;
+		@apply absolute top-3 right-3 opacity-70 transition-opacity hover:opacity-100;
 	}
 
 	.backdrop {
