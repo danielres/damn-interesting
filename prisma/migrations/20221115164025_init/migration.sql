010b8dd743456bb4214213d111eb380638f3d680
Daniel Reszka <dannn.r@gmail.com>
2022-11-16 10:12:46 +0100

    prisma: migrations: collapse into 1 init migration

---
+++ prisma/migrations/20221115164025_init/migration.sql
@@ -0,0 +1,67 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "invitedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "inviterId" TEXT,
+    "role" TEXT NOT NULL DEFAULT 'USER',
+    CONSTRAINT "User_inviterId_fkey" FOREIGN KEY ("inviterId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Entry" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "url" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "ownerId" TEXT NOT NULL,
+    "authorName" TEXT NOT NULL,
+    "authorUrl" TEXT NOT NULL,
+    "height" INTEGER NOT NULL,
+    "providerName" TEXT NOT NULL,
+    "thumbnailHeight" INTEGER NOT NULL,
+    "thumbnailUrl" TEXT NOT NULL,
+    "thumbnailWidth" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "resourceType" TEXT NOT NULL,
+    "width" INTEGER NOT NULL,
+    CONSTRAINT "Entry_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "creatorId" TEXT NOT NULL,
+    CONSTRAINT "Tag_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Tagging" (
+    "entryId" TEXT NOT NULL,
+    "tagId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "creatorId" TEXT NOT NULL,
+
+    PRIMARY KEY ("entryId", "tagId"),
+    CONSTRAINT "Tagging_entryId_fkey" FOREIGN KEY ("entryId") REFERENCES "Entry" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Tagging_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "Tag" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Tagging_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_slug_key" ON "User"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tag_name_key" ON "Tag"("name");
