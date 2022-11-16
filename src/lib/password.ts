944d0b5ea06e97d84e90fb22ab74a33f8da4011e
Daniel Reszka <dannn.r@gmail.com>
2022-11-16 10:11:53 +0100

    lib/password.ts: hash(): use same complexity in dev and in prod

--- src/lib/password.ts
+++ src/lib/password.ts
@@ -3,7 +3,5 @@ import bcrypt from 'bcryptjs'
 export const compare = bcrypt.compare
 
 const isTest = import.meta.env.MODE === 'test'
-const isDev = import.meta.env.MODE === 'development'
 
-export const hash = async (password: string) =>
-	await bcrypt.hash(password, isTest || isDev ? 1 : 10)
+export const hash = async (password: string) => await bcrypt.hash(password, isTest ? 1 : 10)
