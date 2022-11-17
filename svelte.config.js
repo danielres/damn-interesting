8efcdbb8def306df846c082fd27516f6c25f41ae
Daniel Reszka <dannn.r@gmail.com>
2022-11-17 12:16:47 +0100

    svelte.config.js: use adapter-node, enable gzip

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-node'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +14,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({ precompress: true }),
 		alias: {
 			$can: 'src/lib/can.ts',
 			$components: 'src/components',
