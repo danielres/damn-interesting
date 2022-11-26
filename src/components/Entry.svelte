ff91328ca2509df134d780f1c58c7027dd5818b3
Daniel Reszka <dannn.r@gmail.com>
2022-11-26 23:38:14 +0100

    Entry.svelte: render duration

--- src/components/Entry.svelte
+++ src/components/Entry.svelte
@@ -3,6 +3,7 @@
 
 	import Prose from '$components/Prose.svelte'
 	import { format } from '$lib/date'
+	import { formatDuration } from '$lib/time'
 	import { marked } from 'marked'
 
 	export let entry: Entry & { owner: Pick<User, 'slug' | 'username'> }
@@ -22,12 +23,24 @@
 	</h3>
 
 	{#if mode === 'thumbnail'}
-		<a href="/entries/{entry.id}" class="opacity-70 hover:opacity-100" data-sveltekit-prefetch>
+		<a
+			href="/entries/{entry.id}"
+			class="relative opacity-70 hover:opacity-100"
+			data-sveltekit-prefetch
+		>
 			<img
 				src={entry.thumbnailUrl}
 				alt={entry.title}
-				class="aspect-video w-full rounded-lg border-4 border-slate-600 object-cover transition-all hover:border-slate-400 hover:shadow-lg"
+				class="aspect-video w-full rounded-lg border-4 border-slate-600 object-cover transition-all hover:border-slate-400 hover:shadow-lg "
 			/>
+
+			{#if entry.duration}
+				<div
+					class="absolute right-0 bottom-0 rounded-tl-lg rounded-br-lg bg-slate-600 px-2 py-1 text-sm "
+				>
+					{formatDuration(entry.duration)}
+				</div>
+			{/if}
 		</a>
 	{/if}
 
