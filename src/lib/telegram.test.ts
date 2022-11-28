dc06381ef4930b313180cc9bd702f952413a3a47
Daniel Reszka <dannn.r@gmail.com>
2022-11-28 05:15:44 +0100

    lib/telegram.ts: improve + add sendBotPhoto() + getBotUpdates()

--- src/lib/telegram.test.ts
+++ src/lib/telegram.test.ts
@@ -1,10 +1,30 @@
 import { describe, expect, it } from 'vitest'
-import { sendBotMessage } from './telegram'
+import { getBotUpdates, sendBotMessage, sendBotPhoto } from './telegram'
+
+const RUN_TELEGRAM_TESTS = false
+
+const PHOTO_URL =
+	'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/A_focused_kitten_%28Pixabay%29.jpg/640px-A_focused_kitten_%28Pixabay%29.jpg'
 
 describe('sendBotMessage()', () => {
-	it('sends a message', async () => {
-		const res = await sendBotMessage(`Hello from the bot`)
+	it.runIf(RUN_TELEGRAM_TESTS)('sends a message', async () => {
+		const res = await sendBotMessage(
+			`Hello from bot!\n__Markdown supported__: *bold*  **italic**.\n👉 [Reference](https://core.telegram.org/bots/api#formatting-options)`
+		)
+		expect(res?.success).toEqual(true)
+	})
+})
 
+describe('sendBotPhoto()', () => {
+	it.runIf(RUN_TELEGRAM_TESTS)('sends a photo', async () => {
+		const res = await sendBotPhoto(PHOTO_URL, 'Oh! A *cuuute* kitten. Bot loves kittens!')
+		expect(res?.success).toEqual(true)
+	})
+})
+
+describe('getUpdates()', () => {
+	it.runIf(RUN_TELEGRAM_TESTS)('gets latests updates received by the bot', async () => {
+		const res = await getBotUpdates()
 		expect(res?.success).toEqual(true)
 	})
 })
--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -6,11 +6,12 @@ const CHAT_ID = private_env.TELEGRAM_CHAT_ID
 export const sendBotMessage = async (text: string) => {
 	if (!TOKEN || !CHAT_ID) return
 
-	const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`
-	const res = await fetch(url, {
+	const apiUrl = `https://api.telegram.org/bot${TOKEN}/sendMessage`
+	const res = await fetch(apiUrl, {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
 		body: new URLSearchParams({
+			parse_mode: 'markdown',
 			chat_id: CHAT_ID,
 			text,
 		}),
@@ -18,7 +19,51 @@ export const sendBotMessage = async (text: string) => {
 
 	if (!res.ok) {
 		const { error_code, description } = await res.json()
-		console.error(`Error: Telegram request failed, code: ${error_code}: ${description}`)
+		console.log(
+			`Error: sendBotMessage(): Telegram request failed, code: ${error_code}: "${description}"`
+		)
+		return { success: false, error: { code: error_code, description } }
+	}
+
+	return { success: true }
+}
+
+export const sendBotPhoto = async (url: string, caption: string | undefined = undefined) => {
+	if (!TOKEN || !CHAT_ID) return
+
+	const apiUrl = `https://api.telegram.org/bot${TOKEN}/sendPhoto`
+	const res = await fetch(apiUrl, {
+		method: 'POST',
+		body: new URLSearchParams({
+			parse_mode: 'markdown',
+			chat_id: CHAT_ID,
+			...(caption ? { caption } : {}),
+			photo: url,
+		}),
+	})
+
+	if (!res.ok) {
+		const { error_code, description } = await res.json()
+		console.log(
+			`Error: sendBotPhoto(): Telegram request failed, code: ${error_code}: "${description}"`
+		)
+		return { success: false, error: { code: error_code, description } }
+	}
+
+	return { success: true }
+}
+
+export const getBotUpdates = async () => {
+	if (!TOKEN) return
+
+	const apiUrl = `https://api.telegram.org/bot${TOKEN}/getUpdates`
+	const res = await fetch(apiUrl)
+
+	if (!res.ok) {
+		const { error_code, description } = await res.json()
+		console.log(
+			`Error: getUpdates(): Telegram request failed, code: ${error_code}: "${description}"`
+		)
 		return { success: false, error: { code: error_code, description } }
 	}
 
