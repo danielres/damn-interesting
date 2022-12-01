aa07c0bf9d54904e73763fa1b88d8f78e5f0ea15
Daniel Reszka <dannn.r@gmail.com>
2022-12-01 09:13:11 +0100

    lib/telegram.ts: support 3 destination group chats (main, admin, test)

--- src/lib/telegram.test.ts
+++ src/lib/telegram.test.ts
@@ -1,23 +1,28 @@
 import { describe, expect, it } from 'vitest'
-import { getBotUpdates, sendBotMessage, sendBotPhoto } from './telegram'
+import { getBotUpdates, sendBotMessage, sendBotPhoto, type Destination } from './telegram'
 
 const RUN_TELEGRAM_TESTS = false
 
-const PHOTO_URL =
-	'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/A_focused_kitten_%28Pixabay%29.jpg/640px-A_focused_kitten_%28Pixabay%29.jpg'
+const TEST_PHOTO_URL = 'https://placekitten.com/240/240'
+
+const TEST_TEXT = `Hello from bot!\n__Markdown supported__: *bold*  **italic**.\n👉 [Reference](https://core.telegram.org/bots/api#formatting-options)`
+
+const DESTINATION: Destination = 'test_group'
 
 describe('sendBotMessage()', () => {
-	it.runIf(RUN_TELEGRAM_TESTS)('sends a message', async () => {
-		const res = await sendBotMessage(
-			`Hello from bot!\n__Markdown supported__: *bold*  **italic**.\n👉 [Reference](https://core.telegram.org/bots/api#formatting-options)`
-		)
+	it.runIf(RUN_TELEGRAM_TESTS)('sends a message to a destination group', async () => {
+		const res = await sendBotMessage(TEST_TEXT, DESTINATION)
 		expect(res?.success).toEqual(true)
 	})
 })
 
 describe('sendBotPhoto()', () => {
-	it.runIf(RUN_TELEGRAM_TESTS)('sends a photo', async () => {
-		const res = await sendBotPhoto(PHOTO_URL, 'Oh! A *cuuute* kitten. Bot loves kittens!')
+	it.runIf(RUN_TELEGRAM_TESTS)('sends a photo to a destination group', async () => {
+		const res = await sendBotPhoto(
+			TEST_PHOTO_URL,
+			DESTINATION,
+			'Oh! A *cuuute* kitten. Bot loves kittens!'
+		)
 		expect(res?.success).toEqual(true)
 	})
 })
--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -1,20 +1,29 @@
 import { env as private_env } from '$env/dynamic/private'
 
 const TOKEN = private_env.TELEGRAM_BOT_TOKEN
-const CHAT_ID = private_env.TELEGRAM_CHAT_ID
+const CHAT_ID_MAIN = private_env.TELEGRAM_CHAT_ID_MAIN
+const CHAT_ID_TEST = private_env.TELEGRAM_CHAT_ID_TEST
+const CHAT_ID_ADMIN = private_env.TELEGRAM_CHAT_ID_ADMIN
 
-export const sendBotMessage = async (text: string) => {
-	if (!TOKEN || !CHAT_ID) return
+export type Destination = 'test_group' | 'main_group' | 'admin_group'
+
+const getChatId = (destination: Destination) =>
+	destination === 'main_group'
+		? CHAT_ID_MAIN
+		: destination === 'admin_group'
+		? CHAT_ID_ADMIN
+		: CHAT_ID_TEST
+
+export const sendBotMessage = async (text: string, destination: Destination) => {
+	if (!TOKEN) return
+
+	const chat_id = getChatId(destination)
 
 	const apiUrl = `https://api.telegram.org/bot${TOKEN}/sendMessage`
 	const res = await fetch(apiUrl, {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-		body: new URLSearchParams({
-			parse_mode: 'markdown',
-			chat_id: CHAT_ID,
-			text,
-		}),
+		body: new URLSearchParams({ parse_mode: 'markdown', chat_id, text }),
 	})
 
 	if (!res.ok) {
@@ -28,15 +37,21 @@ export const sendBotMessage = async (text: string) => {
 	return { success: true }
 }
 
-export const sendBotPhoto = async (url: string, caption: string | undefined = undefined) => {
-	if (!TOKEN || !CHAT_ID) return
+export const sendBotPhoto = async (
+	url: string,
+	destination: Destination,
+	caption: string | undefined = undefined
+) => {
+	if (!TOKEN || !CHAT_ID_TEST) return
+
+	const chat_id = getChatId(destination)
 
 	const apiUrl = `https://api.telegram.org/bot${TOKEN}/sendPhoto`
 	const res = await fetch(apiUrl, {
 		method: 'POST',
 		body: new URLSearchParams({
 			parse_mode: 'markdown',
-			chat_id: CHAT_ID,
+			chat_id,
 			...(caption ? { caption } : {}),
 			photo: url,
 		}),
