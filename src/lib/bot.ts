b2cdc6771efe884df6c5827e44ec836e38777e20
Daniel Reszka <dannn.r@gmail.com>
2022-12-03 11:59:00 +0100

    lib/bot.ts: handleNewEntry(): fix missing destination group

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -2,8 +2,10 @@ import type { Entry } from '@prisma/client'
 
 import { SITE } from '$constants'
 import { sendBotMessage } from '$lib/telegram'
+import { dev } from '$app/environment'
 
 export const handleNewEntry = (username: string, entry: Entry) =>
 	sendBotMessage(
-		`⚡${username} just posted:⚡\n[${entry.title}](${SITE.base_url}/entries/${entry.id})`
+		`⚡${username} just posted:⚡\n[${entry.title}](${SITE.base_url}/entries/${entry.id})`,
+		dev ? 'test_group' : 'main_group'
 	)
