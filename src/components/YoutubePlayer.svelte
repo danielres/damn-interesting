d480ef81bc0851030dbd4a53e65d89724c9c9af1
Daniel Reszka <dannn.r@gmail.com>
2023-01-06 22:35:03 +0100

    components/: add YoutubePlayer

---
+++ src/components/YoutubePlayer.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { YouTubePlayer as TYoutubePlayer } from 'youtube-player/dist/types'
+
+	import { onMount } from 'svelte'
+	import YouTubePlayer from 'youtube-player'
+
+	export let id: string | undefined
+
+	let _class = ''
+	export { _class as class }
+
+	let player: TYoutubePlayer | undefined
+
+	onMount(() => {
+		player = YouTubePlayer(`player`)
+	})
+
+	$: if (player && id) player.loadVideoById(id)
+</script>
+
+<div id="player" class={_class} />
