9cf647f8c11f2fb3900e37b97896b5f6bbcdece8
Daniel Reszka <dannn.r@gmail.com>
2023-01-06 18:09:48 +0100

    components/: add Spinner

---
+++ src/components/Spinner.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Icon from '$components/Icon.svelte'
+
+	let _class = 'h-5 w-5 text_white'
+	export { _class as class }
+</script>
+
+<Icon type="spinner" class=" animate-spin {_class}" />
