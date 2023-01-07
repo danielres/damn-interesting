9d251dbfd79803c1caf56dcb8c587bd2054e405b
Daniel Reszka <dannn.r@gmail.com>
2023-01-08 00:16:32 +0100

    stores: users: add signout()

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,5 +1,11 @@
+import { invalidateAll } from '$app/navigation'
 import type { CurrentUserView } from '$types'
 
 import { writable } from 'svelte/store'
 
 export const userStore = writable<CurrentUserView | undefined>(undefined)
+
+export const signout = () => {
+	userStore.set(undefined)
+	invalidateAll()
+}
