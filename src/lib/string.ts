4a6714dbcef317784aba0433b89e9f33de722859
Daniel Reszka <dannn.r@gmail.com>
2023-01-13 19:08:06 +0100

    $lib/strings.ts: support undefined
    * applied to capitalizeFirst()
    * applied to sanitizeInputValue()

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -20,9 +20,11 @@ export const slugify = (string: string) => {
 
 export const truncate = (string: string, length = 30) => string.substring(0, length) + '…'
 
-export const capitalizeFirst = (string: string) => string.charAt(0).toUpperCase() + string.slice(1)
+export const capitalizeFirst = (string?: string) =>
+	string ? string.charAt(0).toUpperCase() + string.slice(1) : string
 
-export const sanitizeInputValue = (string: string, { trimEnd } = { trimEnd: true }) => {
+export const sanitizeInputValue = (string?: string, { trimEnd } = { trimEnd: true }) => {
+	if (!string) return string
 	const result = string
 		.trimStart()
 		.replace(/\s\s+/g, ' ') // Collapses multiple spaces
