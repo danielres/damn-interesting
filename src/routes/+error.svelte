92a59302bcc55536d8ee50ab7aadf1ec5dd28b7c
Daniel Reszka <dannn.r@gmail.com>
2023-01-13 23:29:14 +0100

    +error.svelte: add with FormSignin if unauthorized

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.postcss'
+
+	import { page } from '$app/stores'
+	import { HTTP_CODES } from '$constants'
+	import FormSignIn2 from './v2/auth/FormSignIn2.svelte'
+</script>
+
+<div class="my-16 mx-auto max-w-md rounded border-black bg-slate-700  p-8">
+	{#if $page.status === HTTP_CODES.UNAUTHORIZED}
+		<FormSignIn2 />
+	{:else}
+		<h2 class="text-lg">Something went wrong</h2>
+		<h1>Error {$page.status}: {$page.error?.message}</h1>
+	{/if}
+</div>
