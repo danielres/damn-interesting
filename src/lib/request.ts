1a2bc11f975c03989a14bcd0a8b26634e84d9299
Daniel Reszka <dannn.r@gmail.com>
2023-01-13 19:06:08 +0100

    $lib/request.ts: parseFormDataJson(): add

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -2,3 +2,9 @@ export const getFormEntriesFromRequest = async (request: Request) => {
 	const formData = await request.formData()
 	return JSON.parse(JSON.stringify(Object.fromEntries(formData)))
 }
+
+export const parseFormDataJson = async (request: Request) => {
+	const values = (await request.formData()).get('json')
+	if (typeof values !== 'string') return
+	return JSON.parse(values)
+}
