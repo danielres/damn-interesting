e4fad6c963317d33dbf82b1a61a226055af4d9f7
Daniel Reszka <dannn.r@gmail.com>
2023-01-14 16:35:46 +0100

    v2/components/Entries.svelte: add

---
+++ src/routes/v2/components/Entries.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" context="module">
+	import IntersectionObserver from '$components/IntersectionObserver.svelte'
+	import { entries, loadMore, newEntriesCount, page } from '$components2/Entries/$entries'
+	import EntryItem from '../entries/components/EntryItem.svelte'
+</script>
+
+<div class="grid grid-cols-[repeat(auto-fill,_minmax(360px,_1fr))] gap-6">
+	{#each $entries as entry (entry.id)}
+		<EntryItem {entry} />
+	{/each}
+
+	{#if $newEntriesCount && $newEntriesCount > 0}
+		{#key $page}
+			<IntersectionObserver on:intersecting={loadMore} />
+		{/key}
+	{/if}
+</div>
