ec47840184920858e32bc145db315bf3d92dd460
Daniel Reszka <dannn.r@gmail.com>
2023-01-14 16:14:58 +0100

    (v2) components/forms/SchemaErrors.svelte: add

---
+++ src/routes/v2/components/forms/SchemaErrors.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	type Errors =
+		//  Conforms to Zod FlattenedErrors format
+		| undefined
+		| {
+				[x: string]: string[] | undefined
+				[x: number]: string[] | undefined
+				[x: symbol]: string[] | undefined
+		  }
+
+	export let errors: Errors
+</script>
+
+{#if errors}
+	<dl
+		class="shadow-lg; m-0 grid grid-cols-[auto_1fr] gap-2 rounded bg-red-700/50 p-2 text-sm text-white/70"
+	>
+		{#each Object.entries(errors) as [label, messages]}
+			{#if messages}
+				<dt class="font-semibold">{label}:</dt>
+				<dd>
+					<ul>
+						{#each messages as message}
+							<li>{@html message}</li>
+						{/each}
+					</ul>
+				</dd>
+			{/if}
+		{/each}
+	</dl>
+{/if}
